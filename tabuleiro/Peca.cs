35b165df2d4fd7b6f0dfb0fba70bdcf9b214a0b1
Pedro Pontes <pedrinhopontes@hotmail.com>
2024-03-08 01:06:37 -0300

    Fazendo os primeiros testes de movimentacao

--- tabuleiro/Peca.cs
+++ tabuleiro/Peca.cs
@@ -13,5 +13,10 @@
             this.cor = cor;
             this.qteMovimentos = 0;
         }
+
+        public void incrementarQteMovimentos()
+        {
+            qteMovimentos++;
+        }
     }
 }
